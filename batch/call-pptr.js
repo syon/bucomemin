ba4e96100e5b18e5e9be9e4ec89f06581533aae8
syon <syon.xiii@gmail.com>
2019-06-30 17:11:49 +0900

    :zap: scrape with puppeteer

--- batch/call-pptr.js
+++ batch/call-pptr.js
@@ -1,8 +1,32 @@
+const cheerio = require('cheerio')
 const pptr = require('./pptr')
 
 ;(async () => {
   const html = await pptr('https://b.hatena.ne.jp/entry/s/www3.nhk.or.jp/news/html/20190621/k10011963241000.html')
-  require('fs').writeFileSync('aaa.html', html)
+  const $ = cheerio.load(html)
+  const popularItems = $('.js-bookmarks-popular .js-bookmark-item')
+  const populars = {}
+  popularItems.each((i, el) => {
+    const user = $(el).find('.entry-comment-username > a').text()
+    const comment = $(el).find('.js-bookmark-comment').text()
+    const date = $(el).find('.entry-comment-timestamp').text()
+    const tw = $(el).find('.twitter-click > a > span').text()
+    const twitterClicks = tw.replace(/ clicks$/, '')
+    populars[user] = { comment, date, twitterClicks }
+  })
+
+  const recentItems = $('.js-bookmarks-recent .js-bookmark-item')
+  const recents = {}
+  recentItems.each((i, el) => {
+    const user = $(el).find('.entry-comment-username > a').text()
+    const comment = $(el).find('.js-bookmark-comment').text()
+    const date = $(el).find('.entry-comment-timestamp').text()
+    const tw = $(el).find('.twitter-click > a > span').text()
+    const twitterClicks = tw.replace(/ clicks$/, '')
+    recents[user] = { comment, date, twitterClicks }
+  })
+
+  require('fs').writeFileSync('hhh.json', JSON.stringify({ populars, recents }, null, 2))
 })().catch(e => {
   // Deal with the fact the chain failed
 })
--- batch/pptr.js
+++ batch/pptr.js
@@ -1,10 +1,52 @@
 const puppeteer = require('puppeteer')
+const dg = require('debug')('app:pptr')
 
 module.exports = async function(url) {
+  const viewportHeight = 1200
+  const viewportWidth = 1600
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  page.setViewport({ width: viewportWidth, height: viewportHeight })
+  dg('goto', url)
   await page.goto(url)
+  dg('click recent...')
+  await page.click('[data-sort="recent"]')
+  await page.waitFor(3000)
+  await scrollToBottom(page, viewportHeight)
+  dg('load html source...')
   const html = await page.content()
+  dg('browser close...')
   await browser.close()
   return html
+}
+
+/**
+ * https://swet.dena.com/entry/2018/04/26/152326
+ */
+async function scrollToBottom(page, viewportHeight) {
+  const getScrollHeight = () => {
+    return Promise.resolve(document.documentElement.scrollHeight)
+  }
+
+  let scrollHeight = await page.evaluate(getScrollHeight)
+  let currentPosition = 0
+  let scrollNumber = 0
+
+  while (currentPosition < scrollHeight) {
+    scrollNumber += 1
+    const nextPosition = scrollNumber * viewportHeight
+    await page.evaluate(function(scrollTo) {
+      return Promise.resolve(window.scrollTo(0, scrollTo))
+    }, nextPosition)
+    await page
+      .waitForNavigation({ waitUntil: 'networkidle2', timeout: 5000 })
+      .catch(e => console.log('timeout exceed. proceed to next operation'))
+
+    currentPosition = nextPosition
+    console.log(`scrollNumber: ${scrollNumber}`)
+    console.log(`currentPosition: ${currentPosition}`)
+
+    scrollHeight = await page.evaluate(getScrollHeight)
+    console.log(`ScrollHeight ${scrollHeight}`)
+  }
 }
