041fa17d1c565ea3c99b5162fd10dd84e614a99e
syon <syon.xiii@gmail.com>
2019-07-12 02:04:11 +0900

    :sparkles: Cal-heatmap

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,7 @@
           {{ f.name }}
         </a>
       </div>
+      <div id="cal-heatmap"></div>
     </div>
   </section>
 </template>
@@ -21,6 +22,21 @@
 import Hatena from 'js-hatena'
 
 export default {
+  head() {
+    return {
+      script: [
+        { src: '//d3js.org/d3.v3.min.js' },
+        { src: '//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.min.js' },
+        { src: '' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: '//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.css'
+        }
+      ]
+    }
+  },
   data() {
     return {
       count: {},
@@ -37,8 +53,32 @@ export default {
     this.getTotalStars()
     this.getFavorites()
     this.$store.dispatch('profile/load', { user: 'Dy66 ' })
+    this.drawHeatmap()
   },
   methods: {
+    drawHeatmap() {
+      const cal = new window.CalHeatMap()
+      cal.init({
+        domain: 'year',
+        subDomain: 'day',
+        range: 1,
+        weekStartOnMonday: false,
+        cellSize: 12,
+        cellPadding: 3,
+        data: '/data.json',
+        afterLoadData: this.parser,
+        legend: [1, 5, 10],
+        tooltip: true
+      })
+    },
+    parser(data) {
+      const stats = {}
+      Object.entries(data).forEach(([k, v]) => {
+        const timestamp = new Date(k).getTime() / 1000
+        stats[timestamp] = v
+      })
+      return stats
+    },
     async getTotalStars() {
       const userId = 'Dy66'
       const uri = `http://b.hatena.ne.jp/${userId}/`
@@ -67,5 +107,21 @@ export default {
 
 .fav {
   padding: 2px 4px;
+}
+
+.graph-rect.q0 {
+  fill: hsl(360, 14%, 93%); /* hsl(216, 14%, 93%) */
+}
+.graph-rect.q1 {
+  fill: hsl(224, 62%, 72%); /* hsl(80, 62%, 72%) */
+}
+.graph-rect.q2 {
+  fill: hsl(256, 45%, 61%); /* hsl(112, 45%, 61%) */
+}
+.graph-rect.q3 {
+  fill: hsl(276, 63%, 37%); /* hsl(132, 63%, 37%) */
+}
+.graph-rect.q4 {
+  fill: hsl(276, 59%, 24%); /* hsl(132, 59%, 24%) */
 }
 </style>
