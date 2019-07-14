e3d1d2def257060d4b2ec4090b5da5f1ce853858
syon <syon.xiii@gmail.com>
2019-07-14 17:18:16 +0900

    :zap: firebase storage

--- server/hello.js
+++ server/hello.js
@@ -1,8 +1,18 @@
-const cheerio = require('cheerio')
+const debug = require('debug')
+const MyHatebu = require('../batch/myhatebu')
+const { storage } = require('./firebaseAdmin')
 
-module.exports = (req, res, next) => {
-  console.log('////////////////////////////')
-  console.log(cheerio)
-  console.log('////////////////////////////')
+debug.enable('app:*')
+const dg = debug('app:hello')
+const bucket = storage.bucket('gs://bmin-faf7e.appspot.com/')
+
+const user = 'Dy66'
+
+module.exports = async (req, res, next) => {
+  dg('==== hello.js ====')
+  const results = await MyHatebu.getBookmarks({ user })
+  const jsonStr = JSON.stringify(results, null, 2)
+  const file = bucket.file(`users/${user}.json`)
+  await file.save(jsonStr)
   res.end('Hello!')
 }
