d039bb5f84ab0ddbf3811af870dafd044a8cc6b4
syon <syon.xiii@gmail.com>
2019-07-14 17:17:49 +0900

    :package: firebase

---
+++ server/firebaseAdmin.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin')
+const serviceAccount = require('../.secret/serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://bmin-faf7e.firebaseio.com'
+})
+
+const db = admin.firestore()
+const storage = admin.storage()
+
+module.exports = { db, storage }
