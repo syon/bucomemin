8e8d3eaf08fcd3f0aba14381982d5d16d385219c
syon <syon.xiii@gmail.com>
2019-07-30 22:44:50 +0900

    :package: sign-bunny

--- docker/cron.js
+++ docker/cron.js
@@ -2,6 +2,8 @@
 require('dotenv').config()
 const debug = require('debug')
 const CronJob = require('cron').CronJob
+const signBunny = require('sign-bunny')
+
 const dg = debug('app:cron')
 debug.enable('app:*')
 
@@ -10,20 +12,19 @@ const newbieHandler = require('./handler/newbieHandler')
 const bridgeHandler = require('./handler/bridgeHandler')
 
 dg('******** Hello! This is cron.js ********')
+console.log(signBunny('Hello! This is cron.js'))
 
-/**
- * 毎時 30 分 00 秒に実行
- */
-// eslint-disable-next-line
+// https://crontab.guru/
+
 new CronJob(
   '0 30 * * * *', // 毎時 30 分 00 秒
   // '*/10 * * * * *', // 10秒置き
   async () => {
-    dg('\n\nStart cron job...')
+    console.log(signBunny('Start Order Cron'))
     await orderHandler().catch(e => {
       dg(e)
     })
-    dg('Done!', new Date().toLocaleString(), '\n\n')
+    console.log(signBunny(`Done! ${timestamp}`))
   },
   null,
   true
@@ -34,16 +35,17 @@ new CronJob(
  */
 new CronJob(
   // 秒 分 時 日 月 曜
-  '0 45 * * * *', // 毎時 45 分 00 秒
+  '0 40 * * * *', // 毎時 45 分 00 秒
   async () => {
-    dg('\n\n<< Start Newbie Cron >>')
+    console.log(signBunny('Start Newbie Cron'))
     try {
       await newbieHandler()
       await bridgeHandler()
     } catch (e) {
       dg(e)
     }
-    dg('Done!', new Date().toLocaleString(), '\n\n')
+    const timestamp = new Date().toLocaleString()
+    console.log(signBunny(`Done! ${timestamp}`))
   },
   null,
   true
